4b636db433e22f1392c13ad3f819bb38ed2a5af2
yujian <ijedyu@gmail.com>
2026-02-24 11:38:25 +0800

    1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
-import './styles/index.css'
+import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
